302d9dfc661a6a3fe19c5c2b61f672a4f4f67246
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2015-03-14 17:12:38 +0800

    Adding Travis CI and Coveralls

--- lib/mox_api/fake_people.rb
+++ lib/mox_api/fake_people.rb
@@ -1,5 +1,16 @@
 class FakePeople < ApiBaseEntity
-  def self.find
-    retrieve_url get("/gifs/#{id}.json")
+
+  def self.auth_token
+    @auth_token
   end
+
+  def self.count
+    @count
+  end
+
+  def self.find
+    retrieve_url get("/https://mox-kg.herokuapp.com/api/v1/people.json?&#{auth_token}&count=#{count}")
+  end
+  #make a FakePerson request
+
 end
--- mox_api.gemspec
+++ mox_api.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry-remote"
   spec.add_development_dependency "pry-nav"
   spec.add_dependency "httparty"
+  spec.add_development_dependency "coveralls"
 end
